8e06ae806b24701c4d4ca03c0b8973a99445d463
Ditt Namn <johanjoensson@gmail.com>
2023-04-30 10:14:34 +0900

    There is a bug in the PRO code, sometimes it crashes with a singular beta matrix, sometimes the beta matrix contains invalid numbers (eg. NaNs). It helps to simply do a full reorthogobalization when at least one vector has lost orthogonality, but this does not fix the underlying issue.

--- impurityModel/ed/lanczos.py
+++ impurityModel/ed/lanczos.py
@@ -19,15 +19,18 @@ def get_block_Lanczos_matrices(
         converged : bool,
         h_local : bool = False,
         verbose : bool = True,
-        partial_reort : bool = False
+        partial_reort : bool = False,
+        debug_ort: bool = False,
 ):
     krylovSize = h.shape[0]
     eps = np.finfo("float").eps
-    if verbose:
-        t0 = time.perf_counter()
+    t0 = time.perf_counter()
 
-    t_reorth = []
+    t_reorth = 0
     t_estimate = 0
+    t_matmul = 0
+    t_conv = 0
+    t_qr = 0
 
     N = psi0.shape[0]
     n = max(psi0.shape[1], 1)
@@ -45,96 +48,112 @@ def get_block_Lanczos_matrices(
     q = np.zeros((2, N, n), dtype=complex)
     q[1, :, :] = psi0
 
+    if rank == 0 and debug_ort:
+        overlap_file = open('overlap.dat', 'a')
+        overlap_file.write(f"{np.max(np.abs(W[1, : 1]))}  {np.max(np.abs( np.conj(Q.T) @ q[1]))}  {1}\n")
+
     if h_local:
         done = False
         # Run at least 1 iteration (to generate $\alpha_0$).
         # We can also not generate more than N Lanczos vectors, meaning we can take
         # at most N/n steps in total
         for i in range(int(np.ceil(krylovSize / n))):
+            t_h = time.perf_counter()
             wp = h @ q[1]
             wp = comm.reduce(wp, root=0)
+            t_matmul += time.perf_counter() - t_h
 
             if rank == 0:
                 alphas = np.append(alphas, [np.conj(q[1].T) @ wp], axis=0)
                 betas = np.append(betas, [np.zeros((n, n), dtype=complex)], axis=0)
                 wp -= q[1] @ alphas[i] + q[0] @ np.conj(betas[i - 1].T)
 
-                t_s = time.perf_counter()
                 q[0] = q[1]
-                q[1], betas[i] = sp.linalg.qr(wp, mode="economic", overwrite_a = True)
+                t_qr_fact = time.perf_counter()
+                q[1], betas[i] = sp.linalg.qr(wp, mode="economic", overwrite_a = True, check_finite = False)
+                # v, r = sp.linalg.qr(wp, mode="full", overwrite_a = True, check_finite = False)
+                # q[1], betas[i] = v[:,:n], r[:n, :]
+                t_qr += time.perf_counter() - t_qr_fact
                 # try:
-                if i % (100//n) == 0:
-                    done = converged(alphas, betas)
-                # except FloatingPointError:
-                #     alphas = alphas[:-1]
-                #     betas = betas[:-1]
-                #     done = True
+                if i % int(np.ceil(krylovSize/n))//10 == 0:
+                # if True:
+                    t_converged = time.perf_counter()
+                    # done = converged(alphas, betas)
+                    delta = converged(alphas, betas)
+                    done = delta < 1e-6
+                    t_conv += time.perf_counter() - t_converged
 
             done = comm.bcast(done, root=0)
             if done:
                 break
 
             if rank == 0 and partial_reort:
+                t_overlap = time.perf_counter()
                 # Clearly a function
                 ####################
                 w_bar = np.zeros((i + 2, n, n), dtype=complex)
                 w_bar[i + 1, :, :] = np.identity(n)
-                try:
-                    w_bar[i, :, :] = (
-                        eps
-                        * N
-                        * sp.linalg.solve_triangular(np.conj(betas[i].T), betas[0], lower = True)
-                        * np.random.normal(loc=0, scale=0.6, size=(n, n))
+                w_bar[i, :, :] = (
+                    eps
+                    * N
+                    * sp.linalg.solve_triangular(np.conj(betas[i].T), betas[0], lower = True)
+                    * np.random.normal(loc=0, scale=0.6, size=(n, n))
+                )
+                if i > 0:
+                    w_bar[0, :, :] = (
+                        W[1, 1] @ betas[0] + W[1, 0] @ alphas[0] - alphas[i] @ W[1, 0] - betas[i - 1] @ W[0, 0]
                     )
-                    if i > 0:
-                        w_bar[0, :, :] = (
-                            W[1, 1] @ betas[0] + W[1, 0] @ alphas[0] - alphas[i] @ W[1, 0] - betas[i - 1] @ W[0, 0]
-                        )
-                        w_bar[0, :, :] = sp.linalg.solve_triangular(np.conj(betas[i].T), w_bar[0], lower = True)
-                    for j in range(1, i):
-                        w_bar[j, :, :] = (
-                            W[1, j + 1] @ betas[j]
-                            + W[1, j] @ alphas[j]
-                            - alphas[i] @ W[1, j]
-                            + W[1, j - 1] @ np.conj(betas[j - 1].T)
-                            - betas[i - 1] @ W[0, j]
-                        )
-                        w_bar[j, :, :] = sp.linalg.solve_triangular(np.conj(betas[i].T), w_bar[j], lower = True)
+                    w_bar[0, :, :] = sp.linalg.solve_triangular(np.conj(betas[i].T), w_bar[0], lower = True)
+                for j in range(1, i):
+                    w_bar[j, :, :] = (
+                        W[1, j + 1] @ betas[j]
+                        + W[1, j] @ alphas[j]
+                        - alphas[i] @ W[1, j]
+                        + W[1, j - 1] @ np.conj(betas[j - 1].T)
+                        - betas[i - 1] @ W[0, j]
+                    )
+                    w_bar[j, :, :] = sp.linalg.solve_triangular(np.conj(betas[i].T), w_bar[j], lower = True)
 
-                    w_bar[:i, :, :] += eps * (betas[i] + betas[:i]) * np.random.normal(loc=0, scale=0.3, size=(i, n, n))
-                except FloatingPointError:
-                    w_bar[:i+1] = 1
+                w_bar[:i, :, :] += eps * (betas[i] + betas[:i]) * np.random.normal(loc=0, scale=0.3, size=(i, n, n))
                 W_new = np.zeros((2, i + 2, n, n), dtype=complex)
                 W_new[0, : i + 1] = W[1]
                 W_new[1, : i + 2] = w_bar
                 W = W_new
                 ###
-                t_estimate += time.perf_counter() - t_s
+                t_estimate += time.perf_counter() - t_overlap
 
-                # reort = np.any(np.abs(W[1, : i + 1]) > 0.01)
+                if rank ==0 and debug_ort:
+                    overlap_file.write(f"{np.max(np.abs(W[1, : i + 1]))}  {np.max(np.abs( np.conj(Q.T) @ q[1]))}  {delta}\n")
                 reort = np.any(np.abs(W[1, : i + 1]) > np.sqrt(eps))
-                if reort or force_reort is not None:
+                if partial_reort and  (reort or force_reort is not None):
+                    t_ortho = time.perf_counter()
                     n_reort += 1
                     # clearly a function
-                    mask = np.array([np.any(np.abs(m) > eps ** (3 / 4), axis=0) for m in W[1]])
-                    mask[-1:] = False
-                    if force_reort is None:
-                        Qm = Q[:, mask[:-1].flatten()]
-                    else:
-                        force_reort = np.append(force_reort, [[False] * n], axis=0)
-                        Qm = Q[:, np.logical_or(mask, force_reort)[:-1].flatten()]
+                    # mask = np.array([np.any(np.abs(m) > eps ** (3 / 4), axis=1) for m in W[1]])
+                    # mask = np.array([[np.any(np.abs(m) > eps ** (3 / 4))]*n for m in W[1]])
+                    # mask[-1:] = False
+                    # mask = np.array([[True]*n for _ in W[1]])
+                    # mask[-1, :] = False
+                    # if force_reort is None:
+                    #     Qm = Q[:, mask[:-1].flatten()]
+                    # else:
+                    #     force_reort = np.append(force_reort, [[False] * n], axis=0)
+                    #     Qm = Q[:, np.logical_or(mask, force_reort)[:-1].flatten()]
                     q[1] = q[1] @ betas[i]
-                    t_s = time.perf_counter()
-                    q[1] -= Qm @ (np.conj(Qm.T) @ q[1])
-                    t_reorth.append(time.perf_counter() - t_s)
-                    ###
+                    # q[1] -= Qm @ (np.conj(Qm.T) @ q[1])
+                    q[1] -= Q @ (np.conj(Q.T) @ q[1])
 
-                    q[1], betas[i] = sp.linalg.qr(q[1], mode="economic", overwrite_a = False)
+                    q[1], betas[i] = sp.linalg.qr(q[1], mode="economic", overwrite_a = True, check_finite = False)
+                    # v, r = sp.linalg.qr(q[1], mode="full", overwrite_a = True, check_finite = False)
+                    # q[1], betas[i] = v[:,:n], r[:n, :]
 
-                    W[1, mask] = eps * np.random.normal(loc=0, scale=1.5, size=W[1, mask].shape)
-                    W[1, force_reort] = eps * np.random.normal(loc=0, scale=1.5, size=W[1, force_reort].shape)
+                    # W[1, mask] = eps * np.random.normal(loc=0, scale=1.5, size=W[1, mask].shape)
+                    # W[1, force_reort] = eps * np.random.normal(loc=0, scale=1.5, size=W[1, force_reort].shape)
+                    W[1, : i + 1] = eps * np.random.normal(loc=0, scale=1.5, size=W[1, : i + 1].shape)
 
-                    force_reort = mask if reort else None
+                    # force_reort = mask if reort else None
+                    force_reort = True if reort else None
+                    t_reorth += time.perf_counter() - t_ortho
                 Q = np.append(Q, q[1], axis=1)
 
             q[1] = comm.bcast(q[1], root=0)
@@ -161,11 +180,16 @@ def get_block_Lanczos_matrices(
             if converged(alphas, betas):
                 break
 
+    if rank == 0 and debug_ort:
+        overlap_file.write("\n\n")
+        overlap_file.close()
     if rank == 0 and verbose:
         print(f"Breaking after iteration {i}, blocksize = {n}")
-        print(f"Estimating overlap took {t_estimate} seconds")
-        if len(t_reorth) > 0:
-            print(f"Reorthogonalized {n_reort} times")
-            print(f"Reorthogonalizing took {sum(t_reorth)} seconds in total")
-        print(f"time(get_block_Lanczons_matrices) = {time.perf_counter() - t0} seconds.")
+        print(f"Matrix vector multiplication took {t_matmul:.4f} seconds")
+        print(f"Estimating overlap took {t_estimate:.4f} seconds")
+        print(f"Estimating convergence took {t_conv:.4f} seconds")
+        print(f"QR factorization took {t_qr:.4f} seconds")
+        print(f"Reorthogonalized {n_reort} times")
+        print(f"Reorthogonalizing took {t_reorth:.4f} seconds")
+        print(f"time(get_block_Lanczons_matrices) = {time.perf_counter() - t0:.4f} seconds.")
     return alphas, betas
