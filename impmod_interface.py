24f3ce41988d55ce38376b624d5f4272be80fc88
Ditt Namn <johanjoensson@gmail.com>
2024-01-23 21:46:39 +0900

    Better accuracy in fixed peak DC mignt be necessary when incõding unoccupied states.

--- impmod_interface.py
+++ impmod_interface.py
@@ -515,24 +515,24 @@ def fixed_peak_dc(h0_op, dc_struct, rank, verbose, dense_cutoff):
         dc_op = {(((l, s, m), "c"), ((l, s, m), "a")): -dc_trial for m in range(-l, l + 1) for s in range(2)}
         h_op_c = finite.addOps([h0_op, u, dc_op])
         h_op_i = finite.c2i_op(sum_bath_states, h_op_c)
-        h_dict = bu.expand(h_op_i, dense_cutoff=dense_cutoff, de2_min=1e-6)
+        h_dict = bu.expand(h_op_i, dense_cutoff=dense_cutoff, de2_min=1e-8)
         h = bu.build_sparse_matrix(h_op_i, h_dict) if bu.size > dense_cutoff else bu.build_dense_matrix(h_op_i, h_dict)
         e_upper = finite.eigensystem_new(
             h,
             e_max=0,
-            k=1,
-            eigenValueTol=1e-6,
+            k=5,
+            eigenValueTol=0,
             verbose=verbose,
             return_eigvecs=False,
             dense_cutoff=dense_cutoff,
         )
-        h_dict = bl.expand(h_op_i, dense_cutoff=dense_cutoff, de2_min=1e-6)
+        h_dict = bl.expand(h_op_i, dense_cutoff=dense_cutoff, de2_min=1e-8)
         h = bl.build_sparse_matrix(h_op_i, h_dict) if bl.size > dense_cutoff else bl.build_dense_matrix(h_op_i, h_dict)
         e_lower = finite.eigensystem_new(
             h,
             e_max=0,
-            k=1,
-            eigenValueTol=1e-6,
+            k=5,
+            eigenValueTol=0,
             verbose=verbose,
             return_eigvecs=False,
             dense_cutoff=dense_cutoff,
