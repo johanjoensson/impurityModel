Fix calculation of G0
Also cleanup some more MPI code

src/impurityModel/ed/greens_function.py
@@ -457,19 +457,17 @@ def calc_Greens_function_with_offdiag(
         excited_basis_sizes[ei] = excited_basis.size
         hOp.clear_memory()
 
+    # Send calculated Greens functions to root
     if basis.comm.rank == 0:
-        if iw is not None:
-            gs_iw_tmp = np.empty_like(gs_matsubara_block)
-            for sender in eigen_roots:
-                if sender == 0:
-                    continue
+        for sender in eigen_roots:
+            if sender == 0:
+                continue
+            if iw is not None:
+                gs_iw_tmp = np.empty_like(gs_matsubara_block)
                 basis.comm.Recv(gs_iw_tmp, source=sender)
                 gs_matsubara_block += gs_iw_tmp
-        if w is not None:
-            gs_w_tmp = np.empty_like(gs_realaxis_block)
-            for sender in eigen_roots:
-                if sender == 0:
-                    continue
+            if w is not None:
+                gs_w_tmp = np.empty_like(gs_realaxis_block)
                 basis.comm.Recv(gs_w_tmp, source=sender)
                 gs_realaxis_block += gs_w_tmp
     elif eigen_basis.comm.rank == 0:
@@ -478,7 +476,6 @@ def calc_Greens_function_with_offdiag(
         if w is not None:
             basis.comm.Send(gs_realaxis_block, dest=0)
 
-    # Send calculated Greens functions to root
     if iw is not None:
         gs_matsubara_block /= Z
     if w is not None:
@@ -714,9 +711,7 @@ def block_Green(
 
 def block_green_impl(basis, hOp, psi_arr, iws, ws, e, delta, slaterWeightMin, verbose):
     comm = basis.comm
-    rank = 0
-    if comm is not None:
-        rank = comm.rank
+    rank = comm.rank if comm is not None else 0
     matsubara = iws is not None
     realaxis = ws is not None
     N = len(basis)

src/impurityModel/ed/selfenergy.py
@@ -661,8 +661,9 @@ def get_sigma(
 
     res = []
     for block, g in zip(blocks, gs):
+        block_ix = np.ix_(block, block)
         wIs = (omega_mesh + 1j * delta)[:, np.newaxis, np.newaxis] * np.eye(len(block))[np.newaxis, :, :]
-        g0_inv = wIs - hcorr[block, block] - hyb(omega_mesh, v_full[:, block], h_bath, delta)
+        g0_inv = wIs - hcorr[block_ix] - hyb(omega_mesh, v_full[:, block], h_bath, delta)
         res.append(g0_inv - np.linalg.inv(g))
 
     return res