b830b9aa604cf97624fef864b1a6a2343ea7612f
Johan Jönsson <johanjoensson@gmail.com>
2026-03-18 10:57:31 +0100

    Adjust bath fitting
    The fitting can now return bath states outside the allowed
    energy range, use these bath states just as an additional shift of
    the Hybridization function at Ef, and the local Hamiltonian. Sort of
    like an additional double counting term.

--- src/impurityModel/scripts/build_h0.py
+++ src/impurityModel/scripts/build_h0.py
@@ -6,19 +6,18 @@ from rspt2spectra.energies import get_mu
 import matplotlib.pyplot as plt
 import numpy as np
 from argparse import ArgumentParser
-from rspt_utils.read import extract_dat
+from pyRSPthon.read import extract_dat
 from impurityModel.ed.finite import matrixToIOp
 from impurityModel.ed.greens_function import (
     block_diagonalize_hyb,
 )
-from impurityModel.ed.block_structure import (
-    build_block_structure, get_blocks
-)
+from impurityModel.ed.block_structure import build_block_structure, get_blocks
 from impurityModel.ed.edchain import (
     build_H_bath_v,
     build_full_bath,
     build_imp_bath_blocks,
 )
+from impurityModel.ed.utils import matrix_print
 import pickle
 
 from mpi4py import MPI
@@ -42,47 +41,42 @@ def run(
     bath_states_per_orbital: int,
     gamma: float,
     fit_unocc: bool,
-    fit_real: bool,
+    fit_imag: bool,
     prefix: str,
     verbose: bool,
     plot: bool,
     *kwargs,
 ):
     comm = MPI.COMM_WORLD
-    
+
     if comm is not None and comm.rank != 0:
         verbose = False
 
     hyb_dat = extract_dat("hyb", cluster, prefix)
-    hs = parse_matrices(
-        out_file="out", search_phrase="Local hamiltonian", prefix=prefix
-    )
-    ps = parse_matrices(
-        out_file="out", search_phrase="Projection matrix", prefix=prefix
-    )
+    hs = parse_matrices(out_file="out", search_phrase="Local hamiltonian", prefix=prefix)
+    ps = parse_matrices(out_file="out", search_phrase="Projection matrix", prefix=prefix)
     qs = parse_matrices(
         out_file="out",
         search_phrase="Transformation to the local cf basis:",
         prefix=prefix,
     )
     if cluster not in hs:
-        raise RuntimeError(
-            f"Could not extract local hamiltonian for cluster {cluster} from file {prefix}/out."
-        )
+        raise RuntimeError(f"Could not extract local hamiltonian for cluster {cluster} from file {prefix}/out.")
     H_dft = hs[cluster]
     mu = get_mu()
     hyb = hyb_dat.orbitals
     w = hyb_dat.w
 
+    # If transformations to the CF basis were found, use them
+    T = np.eye(H_dft.shape[0], dtype=complex)
     if cluster in qs:
         T = qs[cluster]
-        hyb_cf = np.conjugate(T.T)[None, :, :] @ hyb @ T[None, :, :]
-        H_dft = np.conjugate(T.T) @ H_dft @ T
-    else:
-        hyb_cf = hyb
+    hyb_cf = np.conjugate(T.T)[None] @ hyb @ T[None]
+    H_dft = np.conjugate(T.T) @ H_dft @ T
+
     phase_hyb, Q = block_diagonalize_hyb(hyb_cf)
 
-    block_structure = build_block_structure(phase_hyb, tol=1e-6)
+    block_structure = build_block_structure(phase_hyb, tol=1e-15)
 
     ebs_star, vs_star = fit_hyb(
         w,
@@ -91,8 +85,7 @@ def run(
         bath_states_per_orbital,
         block_structure,
         gamma,
-        not fit_real,
-        (w[0], 0) if fit_unocc else None,
+        (w[0], 0) if not fit_unocc else None,
         verbose,
         comm,
     )
@@ -109,18 +102,52 @@ def run(
                 f"Energy   :  Hopping  (impurity orbitals {block_structure.blocks[block_structure.inequivalent_blocks[bi]]})"
             )
             for eb_i, vb_i in zip(eb, vb):
-                print(f"{eb_i: 9.6f}:  ", "  ".join(f"{val: 9.6f}" for val in vb_i))
+                print(f"{eb_i: 9.6f} :")
+                matrix_print(vb_i)
+                # print(f"{eb_i: 9.6f}:  ", "  ".join(f"{val: 9.6f}" for val in vb_i))
             print("")
         print("=" * 80)
 
-    print(f"{bath_geometry=}")
+    w_min = w[0]
+    w_max = w[-1]
+    if not fit_unocc:
+        w_max = 0
+    filtered_ebs_star, filtered_vs_star = ([], [])
+    shifts = []
+    for ebs, vs in zip(ebs_star, vs_star):
+        shift = 0
+        filtered_ebs = np.empty((0,), dtype=float)
+        filtered_vs = np.empty((0, vs.shape[1], vs.shape[2]), dtype=vs.dtype)
+        for i in range(ebs.shape[0]):
+            eb = ebs[i]
+            v = vs[i]
+            if w_min <= eb <= w_max:
+                filtered_ebs = np.append(filtered_ebs, eb)
+                filtered_vs = np.append(filtered_vs, [v], axis=0)
+                continue
+            shift += np.conj(v.T) @ v / eb
+        filtered_ebs_star.append(filtered_ebs)
+        filtered_vs_star.append(filtered_vs)
+        shifts.append(shift)
+    ebs_star = filtered_ebs_star
+    vs_star = filtered_vs_star
+
+    H_shift = np.zeros_like(H_dft)
+    for inequiv_block_i, shift in zip(block_structure.inequivalent_blocks, shifts):
+        for block_i in block_structure.identical_blocks[inequiv_block_i]:
+            orbs = block_structure.blocks[block_i]
+            H_shift[np.ix_(orbs, orbs)] = shift
+    if verbose:
+        matrix_print(H_shift, r"Shift of $\Delta(\omega=0)$")
+
     H_baths, vs = build_H_bath_v(
-        H_dft,
+        np.conj(Q.T) @ H_dft @ Q - H_shift,
         ebs_star,
         vs_star,
         bath_geometry.lower(),
         block_structure,
         verbose,
+        False,
     )
     H_bath, v = build_full_bath(H_baths, vs, block_structure)
     if comm is not None:
@@ -132,57 +159,62 @@ def run(
     if plot:
         from itertools import product
         import matplotlib.pyplot as plt
-        beta = 10.99
-        iwn = np.arange(-128, 129, 1)
-        iwn = np.array([np.pi/beta*(2*n - 1)*1j for n in iwn])
+
+        # iwn = np.array([np.pi / beta * (2 * n - 1) * 1j for n in iwn])
+        wn = w + 1j * eim
         I = np.eye(H_bath.shape[0])
-        V = v @ np.conj(Q.T)
-        hyb = np.conj(V.T)[None, ...] @ np.linalg.solve(I[None, ...]*iwn[:, None, None] - H_bath[None, ...], V[None, ...])
-        blocks = get_blocks(hyb)
-        print("V = ")
-        print("\n".join(["  ".join([f"{el.real: 5.3f} {el.imag:+5.3f}" for el in row]) for row in V]))
-        print("H_bath = ")
-        print("\n".join(["  ".join([f"{el.real: 5.3f} {el.imag:+5.3f}" for el in row]) for row in H_bath]))
-        print(f"{blocks=}")
-        for block in get_blocks(hyb):
-            fig, ax = plt.subplots(nrows=len(block), ncols=len(block), squeeze=False)
+        # V = v  # @ np.conj(Q.T)
+        hyb = (
+            Q[None]
+            @ np.conj(v.T)[None, ...]
+            @ np.linalg.solve(I[None, ...] * wn[:, None, None] - H_bath[None, ...], v[None, ...])
+            @ np.conj(Q.T)[None]
+        )
+        phase_hyb = Q[None] @ phase_hyb @ np.conj(Q.T)[None]
+        blocks = get_blocks(hyb, tol=0)
+        # blocks = [list(range(10))]
+        for block in blocks:
+            fig, ax = plt.subplots(nrows=len(block), ncols=len(block), squeeze=False)
             for (i, orb_i), (j, orb_j) in product(enumerate(block), repeat=2):
-                ax[i,j].plot(iwn.imag, hyb[:, orb_i,orb_j].real, '-')
-                ax[i,j].plot(iwn.imag, hyb[:, orb_i,orb_j].imag, '--')
-            fig.suptitle(r"$\Delta_{fit}(\omega)$")
+                ax[i, j].fill_between(
+                    wn.real,
+                    phase_hyb[:, orb_i, orb_j].real,
+                    0,
+                    alpha=0.3,
+                    color="tab:blue",
+                )
+                ax[i, j].plot(wn.real, hyb[:, orb_i, orb_j].real, color="tab:blue")
+            fig.suptitle(r"Re$\left\{\Delta_{fit}(\omega)\right\}$")
+            fig, ax = plt.subplots(nrows=len(block), ncols=len(block), squeeze=False)
+            for (i, orb_i), (j, orb_j) in product(enumerate(block), repeat=2):
+                ax[i, j].fill_between(
+                    wn.real,
+                    phase_hyb[:, orb_i, orb_j].imag,
+                    0,
+                    alpha=0.3,
+                    color="tab:blue",
+                )
+                ax[i, j].plot(wn.real, hyb[:, orb_i, orb_j].imag, color="tab:blue")
+            fig.suptitle(r"Im$\left\{\Delta_{fit}(\omega)\right\}$")
         plt.show()
 
-    occupied_indices, positive_indices = partition_index(
-        np.diag(H_bath), pred=lambda x: x < 0
-    )
+    occupied_indices, positive_indices = partition_index(np.diag(H_bath), pred=lambda x: x < 0)
     sorted_bath_indices = np.array(occupied_indices + positive_indices, dtype=int)
-    # sorted_bath_indices = np.argsort(np.diag(H_bath))
     H_bath = H_bath[np.ix_(sorted_bath_indices, sorted_bath_indices)]
     v = v[sorted_bath_indices, :]
     n_orb = H_dft.shape[0]
     H = np.zeros((n_orb + H_bath.shape[0], n_orb + H_bath.shape[0]), dtype=complex)
-    H[:n_orb, :n_orb] = H_dft
+
+    # Transform from block diagonal -> CF -> correlated basis
+    H[:n_orb, :n_orb] = T @ (H_dft - Q @ H_shift @ np.conj(Q.T)) @ np.conj(T.T)
     H[n_orb:, n_orb:] = H_bath
-    H[n_orb:, :n_orb] = v @ np.conj(Q.T)
+    H[n_orb:, :n_orb] = v @ np.conj(Q.T) @ np.conj(T.T)
     H[:n_orb, n_orb:] = np.conj(H[n_orb:, :n_orb].T)
 
-    impurity_indices, valence_bath_indices, conduction_bath_indices, block_structure = (
-        build_imp_bath_blocks(H, n_orb)
-    )
+    impurity_indices, valence_bath_indices, conduction_bath_indices, block_structure = build_imp_bath_blocks(H, n_orb)
 
     if verbose:
-        print("H = ")
-        print(
-            "\n".join(
-                [
-                    " ".join(
-                        [f"{np.real(elem): .6f} {np.imag(elem):+.6f}i" for elem in row]
-                    )
-                    for row in H
-                ]
-            )
-        )
-        print(f"Diag(H) :\n{np.diag(H)}")
+        print(f"eigvals(H) :\n{np.linalg.eigvalsh(H)}")
 
     h_op = matrixToIOp(H)
     write_to_file(h_op, f"{cluster}_h0_op", save_as_dict=True)
@@ -196,10 +228,10 @@ def main():
     parser.add_argument("cluster", type=str)
     parser.add_argument("bath_states_per_orbital", type=int)
     parser.add_argument("-bg", "--bath-geometry", type=str, default="Star")
-    parser.add_argument("--eim", type=float, default=0.10)
-    parser.add_argument("--gamma", type=float, default=0.001)
+    parser.add_argument("--eim", type=float, default=0.010)
+    parser.add_argument("--gamma", type=float, default=0.100)
     parser.add_argument("--fit-unocc", action="store_true", dest="fit_unocc")
-    parser.add_argument("-i", "--imag-only", action="store_true", dest="fit_real")
+    parser.add_argument("-i", "--imag-only", action="store_true", dest="fit_imag")
     parser.add_argument("-d", "--directory", type=str, default=".", dest="prefix")
     parser.add_argument("-v", "--verbose", action="store_true")
     parser.add_argument("-p", "--plot", action="store_true")
